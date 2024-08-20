9754f9b6df2cfa62aa138387120d2210b60da733
Ralph Wang <traveller@163.com>
2024-08-20 12:17:08 +0800

    feat: 创建默认项目

---
+++ src/main/kotlin/wang/ralph/ai/rust/dataset/RustDatasetApplication.kt
@@ -0,0 +1,11 @@
+package wang.ralph.ai.rust.dataset
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RustDatasetApplication
+
+fun main(args: Array<String>) {
+    runApplication<RustDatasetApplication>(*args)
+}
